68960758741d949f1f2b8d333a1aeb5db65770e0
gbelda <gbelda56@gmail.com>
2021-11-02 17:23:11 +0100

    Implemented completed and trash section
    successfully implemented to do cycle functions and clear all

--- ASSETS/JS/main.js
+++ ASSETS/JS/main.js
@@ -4,9 +4,11 @@ const app = new Vue({
         logoImg: 'https://www.boolean.careers/images/misc/logo.png',
         noTask: false,
         error: false,
-        complete: false,
-        completedTasks: [],
+        complete: true,
+        emptyBin: true,
         newTask: "",
+        completedTasks: [],
+        deletedTasks: [],
         tasks: [
             'Fare i compiti',
             'Fare la spesa',
@@ -16,32 +18,49 @@ const app = new Vue({
     },
     methods: {
         removeTask(i) {
+            this.deletedTasks.push(this.tasks[i])
             this.tasks.splice(i, 1)
+            this.emptyBin = false
             if (this.tasks.length == 0) {
                 this.noTask = true
             }
         },
-        checked(i) {
-            if (!this.completedTasks.includes(this.tasks[i])) {
-                this.completedTasks.push(this.tasks[i])
-                console.log(this.completedTasks);
+        checkedTask(i) {
+            this.completedTasks.push(this.tasks[i])
+            this.tasks.splice(i, 1)
+            this.complete = false
 
-            } else {
-                this.completedTasks.splice(this.tasks[i], 1)
-                console.log(this.completedTasks);
-            }
         },
         addTask() {
             if (this.newTask.length > 6) {
                 this.tasks.push(this.newTask)
                 this.error = false
                 this.newTask = ""
-                console.log(this.tasks);
                 this.noTask = false
             } else {
                 this.error = true
             }
         },
+        redoTask(i) {
+            this.tasks.push(this.completedTasks[i])
+            this.completedTasks.splice(i, 1)
+            this.noTask = false
+            if (this.completedTasks.length == 0) {
+                this.complete = true
+            }
+        },
+        restoreTask(i) {
+            this.tasks.push(this.deletedTasks[i])
+            this.deletedTasks.splice(i, 1)
+            this.noTask = false
+            if (this.deletedTasks.length == 0) {
+                this.emptyBin = true
+            }
+        },
+        deleteAll() {
+            this.deletedTasks = []
+            this.emptyBin = true
+        }
 
     },
 
