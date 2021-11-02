a0b547f517d3e5e9633d9e808b836b849f2044e6
gbelda <gbelda56@gmail.com>
2021-11-02 14:25:13 +0100

    Added check if completed
    need debug on click

--- ASSETS/JS/main.js
+++ ASSETS/JS/main.js
@@ -3,6 +3,7 @@ const app = new Vue({
     data: {
         logoImg: 'https://www.boolean.careers/images/misc/logo.png',
         error: false,
+        completedTasks: [],
         tasks: [
             'Fare i compiti',
             'Fare la spesa',
@@ -16,8 +17,18 @@ const app = new Vue({
             if (this.tasks.length == 0) {
                 return this.error = true
             }
-        }
-    }
+        },
+        checked(i) {
+            if (!this.completedTasks.includes(i)) {
+                this.completedTasks.push(i)
+                console.log(this.completedTasks);
+            } else {
+                this.completedTasks.splice(i, 1)
+                console.log(this.completedTasks);
+            }
+        },
+    },
+
 
 })
 
