00c74e97e30bc07830061ff13fc1e06ca7768d5a
gbelda <gbelda56@gmail.com>
2021-11-02 12:35:22 +0100

    Added error messaghe when to do is empty

--- ASSETS/JS/main.js
+++ ASSETS/JS/main.js
@@ -2,14 +2,21 @@ const app = new Vue({
     el: '#root',
     data: {
         logoImg: 'https://www.boolean.careers/images/misc/logo.png',
+        error: false,
         tasks: [
             'Fare i compiti',
             'Fare la spesa',
             'Fare il bucato'
-        ]
+        ],
 
-
-
+    },
+    methods: {
+        removeTask(i) {
+            this.tasks.splice(i, 1)
+            if (this.tasks.length == 0) {
+                return this.error = true
+            }
+        }
     }
 
 })
