e0ae26511f13fb24e8ccd9ccf6b7dd26d4bdc52e
gbelda <gbelda56@gmail.com>
2021-11-02 12:01:50 +0100

    Added base html elements

--- ASSETS/JS/main.js
+++ ASSETS/JS/main.js
@@ -1,6 +1,13 @@
 const app = new Vue({
     el: '#root',
     data: {
+        logoImg: 'https://www.boolean.careers/images/misc/logo.png',
+        tasks: [
+            'Fare i compiti',
+            'Fare la spesa',
+            'Fare il bucato'
+        ]
+
 
 
     }
